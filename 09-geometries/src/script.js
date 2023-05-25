00cf06e13e6c1c19a7d132abc09f94273ef164e8
Guy Kozlovskij <gajus.kozlovskij@gmail.com>
2023-05-25 16:28:03 +0100

    finish lesson 9

--- 09-geometries/src/script.js
+++ 09-geometries/src/script.js
@@ -10,13 +10,19 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-const positionsArray = new Float32Array([
-  0, 0, 0,
-  0, 1, 0,
-  1, 0, 0
-])
+const geometry = new THREE.BufferGeometry()
+
+const count = 50
+const positionsArray = new Float32Array(count * 3 * 3)
+
+for (let i = 0; i < count * 3 * 3; i++) {
+  positionsArray[i] = Math.random() - 0.5
+}
+
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+geometry.setAttribute('position', positionsAttribute)
+
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1, 20, 20, 20)
 const material = new THREE.MeshBasicMaterial({
   color: 0xff0000,
   wireframe: true,
